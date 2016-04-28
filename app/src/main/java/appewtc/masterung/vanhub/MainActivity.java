8bf725cc6aacceb0c8fc23decd2e142689e68067
masterUNG <phrombutr@gmail.com>
2016-04-28 13:09:23 +0700

    Get JSON String finsihs

--- app/src/main/java/appewtc/masterung/vanhub/MainActivity.java
+++ app/src/main/java/appewtc/masterung/vanhub/MainActivity.java
@@ -2,9 +2,15 @@ package appewtc.masterung.vanhub;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +33,46 @@ public class MainActivity extends AppCompatActivity {
         //Delete SQLite
         deleteSQLite();
 
+        //Synchronize JSON to SQLite
+        synJSON();
 
     }   // Main Method
+
+    //Create Inner Class
+    public class ConnectedJSON extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/van/php_get_user_master.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("28April", "JSON ===>>> " + s);
+
+        }   // onPost
+    }   // Connected Class
+
+    private void synJSON() {
+
+        ConnectedJSON connectedJSON = new ConnectedJSON();
+        connectedJSON.execute();
+
+    }   // synJSON
 
     private void deleteSQLite() {
         SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
